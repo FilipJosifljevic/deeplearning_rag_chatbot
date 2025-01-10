75c5e994b7317639cee2a4a86ff511554d89a42b
Marina-Marinkovic <marinkovicmakica@gmail.com>
2025-01-10 15:12:00 +0100

    frontend komunikacija sa backendom + docker i nginx fil-ovi

--- frontend-rag-deep-learning-chatbot/src/app/chat/chat.component.ts
+++ frontend-rag-deep-learning-chatbot/src/app/chat/chat.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ChatService } from '../chat.service';
+import { FileService } from '../file.service';
 
 @Component({
   selector: 'app-chat',
@@ -12,26 +14,49 @@ import { FormsModule } from '@angular/forms';
 export class ChatComponent {
   messages: { text: string; isUser: boolean }[] = [];
   newMessage: string = '';
+  chatService!: ChatService;
+  fileService!: FileService;
 
   sendMessage(): void {
     if (this.newMessage.trim()) {
       this.messages.push({ text: this.newMessage, isUser: true });
+      this.chatService.sendQuery(this.newMessage.trim()).subscribe(
+        (response) => {
+          this.messages.push({ text: response.answer, isUser: false }); // Odgovor agenta
+          console.log('odgovor agenta');
+        },
+        (error) => {
+          console.error('Error fetching response:', error);
+          this.messages.push({ text: 'Error: Unable to fetch response.', isUser: false });
+        }
+      );
       this.newMessage = '';
-
-      // Simulate agent response
-      setTimeout(() => {
-        this.messages.push({ text: 'This is a response from the agent.', isUser: false });
-      }, 1000);
+      
+     // setTimeout(() => {
+     //   this.messages.push({ text: 'This is a response from the agent.', isUser: false });
+     // }, 1000);
     }
   }
 
+  
+  
   uploadFile(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
       console.log('File uploaded:', file);
 
-      // Here, send the file to the backend as needed
+      this.fileService.uploadFile(file).subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.error('Error during file upload:', error);
+        }
+      );
     }
+
   }
+  
 }
+
